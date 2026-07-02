0e10dc27ad0031b1043e56cbf8ba82cfa12d063c
fponcesu-sketch <fponcesu@gmail.com>
2026-07-02 16:26:07 +0200

    Load decision flow styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './prototype.css';
+import './decisionFlow.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
