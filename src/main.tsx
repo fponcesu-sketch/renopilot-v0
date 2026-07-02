4e4e689f5b41f03e3618732ccb3c0abd883a468c
fponcesu-sketch <fponcesu@gmail.com>
2026-07-02 17:53:44 +0200

    Load UI overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './prototype.css';
 import './decisionFlow.css';
+import './uiOverrides.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
